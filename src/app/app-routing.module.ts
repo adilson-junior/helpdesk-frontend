fcd1a923c47d6962ed451501c774dce7136dd423
adilson-junior <adilsonvajr@gmail.com>
2022-01-22 16:21:51 -0300

    Criado o componente header e implementado o html e css dele

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 
 //Aqui definimos toda a rota do nosso sistema
 const routes: Routes = [
   {
     //defino que toda rota vazia /'' será direcionado para esse componente.
-    path: '',
-    component: NavComponent
+    path: '', component: NavComponent,
+      children: [
+        { path: 'home', component: HomeComponent }
+      ]
   }
 ];
 
