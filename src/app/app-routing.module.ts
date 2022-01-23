1efa0c17f6b173f0a6b31a828ae405138991cf49
adilson-junior <adilsonvajr@gmail.com>
2022-01-23 07:43:09 -0300

    Criado o componente login e implementado seu html e css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 //Aqui definimos toda a rota do nosso sistema
 const routes: Routes = [
-  {
+  { path: 'login', component: LoginComponent},
+  {    
     //defino que toda rota vazia /'' será direcionado para esse componente.
     path: '', component: NavComponent,
       children: [
