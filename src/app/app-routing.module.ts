6afc242e92e085c3d7828eb79e089235d0afc1ea
adilson-junior <adilsonvajr@gmail.com>
2022-01-23 07:19:14 -0300

    Criado o componente tecnico, o modelo e seu html e css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 //Aqui definimos toda a rota do nosso sistema
 const routes: Routes = [
@@ -9,7 +10,8 @@ const routes: Routes = [
     //defino que toda rota vazia /'' será direcionado para esse componente.
     path: '', component: NavComponent,
       children: [
-        { path: 'home', component: HomeComponent }
+        { path: 'home', component: HomeComponent },
+        { path: 'tecnicos', component: TecnicoListComponent}
       ]
   }
 ];
